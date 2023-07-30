1dd5b0799ad910bf415fc880ee76a028a04737c9
Normandia Akbar <normnd.akbr2@gmail.com>
2023-07-31 01:49:59 +0800

    Update index.php

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -11,11 +11,11 @@
                 <div class="card-body">
                     <div class="row align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-uppercase mb-1">Earnings (Monthly)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            <div class="text-xs font-weight-bold text-uppercase mb-1">Project On-Going</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">30</div>
                             <div class="mt-2 mb-0 text-muted text-xs">
                                 <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                                <span>Since last month</span>
+                                <span>improvement</span>
                             </div>
                         </div>
                         <div class="col-auto">
@@ -31,7 +31,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-uppercase mb-1">Sales</div>
+                            <div class="text-xs font-weight-bold text-uppercase mb-1">Data</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">650</div>
                             <div class="mt-2 mb-0 text-muted text-xs">
                                 <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
@@ -39,7 +39,7 @@
                             </div>
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-shopping-cart fa-2x text-success"></i>
+                            <i class="fas fa-database fa-2x text-success"></i>
                         </div>
                     </div>
                 </div>
@@ -51,7 +51,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-uppercase mb-1">New User</div>
+                            <div class="text-xs font-weight-bold text-uppercase mb-1">Data</div>
                             <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">366</div>
                             <div class="mt-2 mb-0 text-muted text-xs">
                                 <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20.4%</span>
@@ -90,7 +90,7 @@
         <div class="col-xl-8 col-lg-7">
             <div class="card mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Monthly Recap Report</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">PPA Dashboard</h6>
                     <div class="dropdown no-arrow">
                         <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
@@ -115,23 +115,23 @@
         <div class="col-xl-4 col-lg-5">
             <div class="card mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Products Sold</h6>
-                    <div class="dropdown no-arrow">
+                    <h6 class="m-0 font-weight-bold text-primary">Waste Water Quality</h6>
+                    <!-- <div class="dropdown no-arrow">
                         <a class="dropdown-toggle btn btn-primary btn-sm" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Month <i class="fas fa-chevron-down"></i>
+                            pH <i class="fas fa-chevron-down"></i>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                            <div class="dropdown-header">Select Periode</div>
-                            <a class="dropdown-item" href="#">Today</a>
-                            <a class="dropdown-item" href="#">Week</a>
-                            <a class="dropdown-item active" href="#">Month</a>
-                            <a class="dropdown-item" href="#">This Year</a>
+                            <div class="dropdown-header">Select Data</div>
+                            <a class="dropdown-item" href="#">pH</a>
+                            <a class="dropdown-item" href="#">TSS</a>
+                            <a class="dropdown-item active" href="#">Fe</a>
+                            <a class="dropdown-item" href="#">Mn</a>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
                 <div class="card-body">
                     <div class="mb-3">
-                        <div class="small text-gray-500">Oblong T-Shirt
+                        <div class="small text-gray-500">by pH
                             <div class="small float-right"><b>600 of 800 Items</b></div>
                         </div>
                         <div class="progress" style="height: 12px;">
@@ -139,7 +139,7 @@
                         </div>
                     </div>
                     <div class="mb-3">
-                        <div class="small text-gray-500">Gundam 90'Editions
+                        <div class="small text-gray-500">by TSS
                             <div class="small float-right"><b>500 of 800 Items</b></div>
                         </div>
                         <div class="progress" style="height: 12px;">
@@ -147,7 +147,7 @@
                         </div>
                     </div>
                     <div class="mb-3">
-                        <div class="small text-gray-500">Rounded Hat
+                        <div class="small text-gray-500">by Fe
                             <div class="small float-right"><b>455 of 800 Items</b></div>
                         </div>
                         <div class="progress" style="height: 12px;">
@@ -155,19 +155,11 @@
                         </div>
                     </div>
                     <div class="mb-3">
-                        <div class="small text-gray-500">Indomie Goreng
+                        <div class="small text-gray-500">by Mn
                             <div class="small float-right"><b>400 of 800 Items</b></div>
                         </div>
                         <div class="progress" style="height: 12px;">
                             <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <div class="small text-gray-500">Remote Control Car Racing
-                            <div class="small float-right"><b>200 of 800 Items</b></div>
-                        </div>
-                        <div class="progress" style="height: 12px;">
-                            <div class="progress-bar bg-success" role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                     </div>
                 </div>
@@ -180,54 +172,54 @@
         <div class="col-xl-8 col-lg-7 mb-4">
             <div class="card">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Invoice</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">I-GSC Document Management</h6>
                     <a class="m-0 float-right btn btn-danger btn-sm" href="#">View More <i class="fas fa-chevron-right"></i></a>
                 </div>
                 <div class="table-responsive">
                     <table class="table align-items-center table-flush">
                         <thead class="thead-light">
                             <tr>
-                                <th>Order ID</th>
-                                <th>Customer</th>
-                                <th>Item</th>
+                                <th>No</th>
+                                <th>Jenis</th>
+                                <th>Total Dokumen</th>
                                 <th>Status</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td><a href="#">RA0449</a></td>
-                                <td>Udin Wayang</td>
-                                <td>Nasi Padang</td>
-                                <td><span class="badge badge-success">Delivered</span></td>
+                                <td><a href="#">1</a></td>
+                                <td>Policy</td>
+                                <td>134</td>
+                                <td><span class="badge badge-success">Updated</span></td>
                                 <td><a href="#" class="btn btn-sm btn-primary">Detail</a></td>
                             </tr>
                             <tr>
-                                <td><a href="#">RA5324</a></td>
-                                <td>Jaenab Bajigur</td>
-                                <td>Gundam 90' Edition</td>
-                                <td><span class="badge badge-warning">Shipping</span></td>
+                                <td><a href="#">2</a></td>
+                                <td>Manual</td>
+                                <td>326</td>
+                                <td><span class="badge badge-success">Updated</span></td>
                                 <td><a href="#" class="btn btn-sm btn-primary">Detail</a></td>
                             </tr>
                             <tr>
-                                <td><a href="#">RA8568</a></td>
-                                <td>Rivat Mahesa</td>
-                                <td>Oblong T-Shirt</td>
-                                <td><span class="badge badge-danger">Pending</span></td>
+                                <td><a href="#">3</a></td>
+                                <td>Bussiness Process</td>
+                                <td>89</td>
+                                <td><span class="badge badge-info">Pending Upload</span></td>
                                 <td><a href="#" class="btn btn-sm btn-primary">Detail</a></td>
                             </tr>
                             <tr>
-                                <td><a href="#">RA1453</a></td>
-                                <td>Indri Junanda</td>
-                                <td>Hat Rounded</td>
-                                <td><span class="badge badge-info">Processing</span></td>
+                                <td><a href="#">4</a></td>
+                                <td>KPI's</td>
+                                <td>220</td>
+                                <td><span class="badge badge-info">Pending Upload</span></td>
                                 <td><a href="#" class="btn btn-sm btn-primary">Detail</a></td>
                             </tr>
                             <tr>
-                                <td><a href="#">RA1998</a></td>
-                                <td>Udin Cilok</td>
-                                <td>Baby Powder</td>
-                                <td><span class="badge badge-success">Delivered</span></td>
+                                <td><a href="#">5</a></td>
+                                <td>SOP (Standar)</td>
+                                <td>112</td>
+                                <td><span class="badge badge-success">Updated</span></td>
                                 <td><a href="#" class="btn btn-sm btn-primary">Detail</a></td>
                             </tr>
                         </tbody>
@@ -240,28 +232,28 @@
         <div class="col-xl-4 col-lg-5 ">
             <div class="card">
                 <div class="card-header py-4 bg-primary d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-light">Message From Customer</h6>
+                    <h6 class="m-0 font-weight-bold text-light">Other User's Feedback</h6>
                 </div>
                 <div>
                     <div class="customer-message align-items-center">
                         <a class="font-weight-bold" href="#">
                             <div class="text-truncate message-title">Hi there! I am wondering if you can help me with a
                                 problem I've been having.</div>
-                            <div class="small text-gray-500 message-time font-weight-bold">Udin Cilok · 58m</div>
+                            <div class="small text-gray-500 message-time font-weight-bold">M. Sapto Nugroho · 58m</div>
                         </a>
                     </div>
                     <div class="customer-message align-items-center">
-                        <a href="#">
+                        <a class="font-weight-bold" href="#">
                             <div class="text-truncate message-title">But I must explain to you how all this mistaken idea
                             </div>
-                            <div class="small text-gray-500 message-time">Nana Haminah · 58m</div>
+                            <div class="small text-gray-500 message-time font-weight-bold">Nelda Tri Rahayu · 58m</div>
                         </a>
                     </div>
                     <div class="customer-message align-items-center">
                         <a class="font-weight-bold" href="#">
                             <div class="text-truncate message-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit
                             </div>
-                            <div class="small text-gray-500 message-time font-weight-bold">Jajang Cincau · 25m</div>
+                            <div class="small text-gray-500 message-time font-weight-bold">Inggit WM · 25m</div>
                         </a>
                     </div>
                     <div class="customer-message align-items-center">
@@ -269,11 +261,11 @@
                             <div class="text-truncate message-title">At vero eos et accusamus et iusto odio dignissimos
                                 ducimus qui blanditiis
                             </div>
-                            <div class="small text-gray-500 message-time font-weight-bold">Udin Wayang · 54m</div>
+                            <div class="small text-gray-500 message-time font-weight-bold">Saring Cipto · 54m</div>
                         </a>
                     </div>
                     <div class="card-footer text-center">
-                        <a class="m-0 small text-primary card-link" href="#">View More <i class="fas fa-chevron-right"></i></a>
+                        <a class="m-0 small text-primary card-link" href="#">Tampillkan Semua <i class="fas fa-chevron-right"></i></a>
                     </div>
                 </div>
             </div>
